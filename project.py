b4dafe077b3a598f1a4060ee10c2a67614b9ff93
RamnikDhoot <99842947+RamnikDhoot@users.noreply.github.com>
2023-12-18 21:37:34 +0000

    made funtion to work with weather api and fetch data (currently not working)

--- project.py
+++ project.py
@@ -5,6 +5,45 @@ from platform import system
 from platform import version
 from platform import python_implementation, python_version_tuple
 import os
+import requests
+
+
+
+def get_weather(city, api_key):
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    complete_url = base_url + "appid=" + api_key + "&q=" + city
+    response = requests.get(complete_url)
+    weather_data = response.json()
+
+    if weather_data['cod'] != 404:
+        main_data = weather_data['main']
+        temperature = main_data['temp'] - 273.15 
+        pressure = main_data['pressure']
+        humidity = main_data['humidity']
+        weather_description = weather_data['weather'][0]['description']
+
+        weather_report = (f"Temperature: {temperature:.2f}°C\n"
+                          f"Atmospheric Pressure: {pressure} hPa\n"
+                          f"Humidity: {humidity}%\n"
+                          f"Description: {weather_description.capitalize()}")
+    else:
+        weather_report = "City Not Found!"
+
+    return weather_report
+
+def main():
+    api_key = "test"  
+    city = input("Enter city name: ")
+    print("\nWeather Report:")
+    print(get_weather(city, api_key))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
 
 
 dir(os)
@@ -39,3 +78,5 @@ print(version())
 print(python_implementation())
 for atr in python_version_tuple():
     print(atr)
+
+
