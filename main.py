6ed79333611a19af8b048fe623b9353384b04882
RamnikDhoot <99842947+RamnikDhoot@users.noreply.github.com>
2023-12-19 20:20:07 +0000

    began work on graphical interface for this weather project using tkinter

--- main.py
+++ main.py
@@ -1,2 +1,80 @@
 import module
 #When a module is imported, its contents are implicitly executed by Python. It gives the module the chance to initialize some of its internal aspects
+
+import requests
+import tkinter as tk
+from tkinter import messagebox
+from datetime import datetime
+
+def fetch_weather(city, api_key, unit, forecast=False):
+    base_url = "http://api.openweathermap.org/data/2.5/"
+    url_type = "forecast" if forecast else "weather"
+    unit_str = '°C' if unit == 'metric' else '°F'
+    complete_url = f"{base_url}{url_type}?appid={api_key}&q={city}&units={unit}"
+
+    try:
+        response = requests.get(complete_url)
+        data = response.json()
+
+        if data['cod'] != "404":
+            if forecast:
+                report = "5-Day Weather Forecast:\n"
+                for item in data['list']:
+                    date_time = datetime.fromtimestamp(item['dt']).strftime('%Y-%m-%d %H:%M:%S')
+                    temperature = item['main']['temp']
+                    description = item['weather'][0]['description']
+                    report += (f"{date_time}: Temp: {temperature:.2f}{unit_str}, "
+                               f"Description: {description.capitalize()}\n")
+            else:
+                main_data = data['main']
+                temperature = main_data['temp']
+                pressure = main_data['pressure']
+                humidity = main_data['humidity']
+                description = data['weather'][0]['description']
+                report = (f"Temperature: {temperature:.2f}{unit_str}\n"
+                          f"Atmospheric Pressure: {pressure} hPa\n"
+                          f"Humidity: {humidity}%\n"
+                          f"Description: {description.capitalize()}")
+        else:
+            report = "City Not Found!"
+
+        return report
+
+    except requests.RequestException as e:
+        return f"Error: {e}"
+
+def display_weather():
+    city = city_entry.get()
+    unit = 'metric' if var.get() == 1 else 'imperial'
+    forecast = forecast_var.get() == 1
+    weather_report = fetch_weather(city, "YOUR_API_KEY", unit, forecast)
+    result_label.config(text=weather_report)
+
+# GUI setup
+root = tk.Tk()
+root.title("Weather App")
+
+# City entry
+tk.Label(root, text="City:").pack()
+city_entry = tk.Entry(root)
+city_entry.pack()
+
+# Temperature unit radio buttons
+var = tk.IntVar()
+var.set(1)
+tk.Radiobutton(root, text="Celsius", variable=var, value=1).pack()
+tk.Radiobutton(root, text="Fahrenheit", variable=var, value=2).pack()
+
+# Forecast checkbox
+forecast_var = tk.IntVar()
+tk.Checkbutton(root, text="5-Day Forecast", variable=forecast_var).pack()
+
+# Submit button
+tk.Button(root, text="Get Weather", command=display_weather).pack()
+
+# Result label
+result_label = tk.Label(root, text="", justify=tk.LEFT)
+result_label.pack()
+
+# Run the application
+root.mainloop()
--- module.py
+++ module.py
@@ -4,29 +4,86 @@ print(__name__)
     #when you run a file directly, its __name__ variable is set to __main__;
     #when a file is imported as a module, its __name__ variable is set to the file's name (excluding .py)
 
-__counter = 0
- 
- 
-def suml(the_list):
-  global __counter
-  __counter += 1
-  the_sum = 0
-  for element in the_list:
-   the_sum += element
-  return the_sum
- 
- 
-def prodl(the_list):
-  global __counter
-  __counter += 1
-  prod = 1
-  for element in the_list:
-   prod *= element
-  return prod
- 
- 
+import os
+import requests
+from datetime import datetime
+
+def get_weather(city, api_key, unit='metric'):
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    unit_str = '°C' if unit == 'metric' else '°F'
+    complete_url = f"{base_url}appid={api_key}&q={city}&units={unit}"
+
+    try:
+        response = requests.get(complete_url)
+        weather_data = response.json()
+
+        if weather_data['cod'] != 404:
+            main_data = weather_data['main']
+            temperature = main_data['temp']
+            pressure = main_data['pressure']
+            humidity = main_data['humidity']
+            weather_description = weather_data['weather'][0]['description']
+            wind_speed = weather_data['wind']['speed']
+            sunrise_time = datetime.fromtimestamp(weather_data['sys']['sunrise']).strftime('%H:%M:%S')
+            sunset_time = datetime.fromtimestamp(weather_data['sys']['sunset']).strftime('%H:%M:%S')
+
+            weather_report = (f"Temperature: {temperature:.2f}{unit_str}\n"
+                              f"Atmospheric Pressure: {pressure} hPa\n"
+                              f"Humidity: {humidity}%\n"
+                              f"Description: {weather_description.capitalize()}\n"
+                              f"Wind Speed: {wind_speed} m/s\n"
+                              f"Sunrise: {sunrise_time}\n"
+                              f"Sunset: {sunset_time}")
+        else:
+            weather_report = "City Not Found!"
+
+        return weather_report
+
+    except requests.RequestException as e:
+        return f"Error retrieving weather data: {e}"
+
+def get_weather_forecast(city, api_key, unit='metric'):
+    base_url = "http://api.openweathermap.org/data/2.5/forecast?"
+    unit_str = '°C' if unit == 'metric' else '°F'
+    complete_url = f"{base_url}appid={api_key}&q={city}&units={unit}"
+
+    try:
+        response = requests.get(complete_url)
+        forecast_data = response.json()
+
+        if forecast_data['cod'] != "404":
+            forecast_report = "5-Day Weather Forecast:\n"
+            for item in forecast_data['list']:
+                date_time = datetime.fromtimestamp(item['dt']).strftime('%Y-%m-%d %H:%M:%S')
+                temperature = item['main']['temp']
+                description = item['weather'][0]['description']
+                forecast_report += (f"{date_time}: Temp: {temperature:.2f}{unit_str}, "
+                                    f"Description: {description.capitalize()}\n")
+        else:
+            forecast_report = "City Not Found!"
+
+        return forecast_report
+
+    except requests.RequestException as e:
+        return f"Error retrieving weather forecast: {e}"
+
+def main():
+    api_key = "test"
+    city = input("Enter city name: ")
+    unit = input("Choose temperature unit - Celsius (C) or Fahrenheit (F): ").lower()
+    unit = 'metric' if unit == 'c' else 'imperial'
+
+    choice = input("Choose option: 1. Current Weather 2. 5-Day Forecast\n")
+
+    if choice == '1':
+        print("\nCurrent Weather Report:")
+        print(get_current_weather(city, api_key, unit))
+    elif choice == '2':
+        print("\nWeather Forecast:")
+        print(get_weather_forecast(city, api_key, unit))
+    else:
+        print("Invalid choice. Please select 1 or 2.")
+
 if __name__ == "__main__":
-  print("I prefer to be a module, but I can do some tests for you.")
-  my_list = [i+1 for i in range(5)]
-  print(suml(my_list) == 15)
-  print(prodl(my_list) == 120)
+    main()
+
